4b7a3e3becf0950e92d99e86abe0b51228b1d8bc
wquynguy <wquyenle@gmail.com>
2024-01-25 12:33:40 -0600

    modified contents and npm run build installed

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -2,12 +2,12 @@ import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) => {
   return (
-    <div style={{ height: '100vh' }}>
-    <PrettyChatWindow
-      projectId="583595a8-2954-4eba-9701-4a1a320a4074"
-      username={props.user.username}
-      secret={props.user.secret}
-      style={{ height: '100vh'}}
+    <div style={{ height: '90vh', backgroundColor: '#22c1c3', padding: '20px', fontFamily: 'Avenir' }}>
+      <PrettyChatWindow
+        projectId="583595a8-2954-4eba-9701-4a1a320a4074"
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: '100vh' }}
       />
     </div>
   );
