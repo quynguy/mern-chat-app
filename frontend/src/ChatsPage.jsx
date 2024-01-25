fe7bbe9f30edb5bbc7f2a8814903addf8bff956c
wquynguy <wquyenle@gmail.com>
2024-01-25 09:38:55 -0600

    installed react-chat-engine-advanced and added props to chatspage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,16 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        "583595a8-2954-4eba-9701-4a1a320a4074",
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }}/>
+        </div>
+    )
   };
   export default ChatsPage;
